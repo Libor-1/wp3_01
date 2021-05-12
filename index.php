38ab90ca48a7eabc4bd9a7c17ce8c48a25e90f51
Libor-1 <60381836+Libor-1@users.noreply.github.com>
2021-05-12 17:25:59 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require_once ('functions.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php getCarDescription(2010) ?>
+</body>
+</html>
